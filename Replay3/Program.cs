5fb9a43b5a166839c59cdc85d57caded0b986f5b
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-11-01 15:40:19 +0200

    Replay3
    Replay3

---
+++ Replay3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Replay3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            String lastName;
+            Console.WriteLine("Enter your naem and last name");
+            name = Console.ReadLine();
+            lastName = Console.ReadLine();
+            Console.WriteLine(name + lastName);
+        }
+    }
+}
